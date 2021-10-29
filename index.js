f465ad289a7d14d57724a25dbb551c622b036a60
Abdullah Mamun <hello.abdullahmamun1@gmail.com>
2021-10-30 02:26:55 +0600

    get all destinations added

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
+const { MongoClient } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
-//GET API
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hqjve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-//POST API
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connected');
+
+        const database = client.db("travelService");
+        const destinationCollection = database.collection("destinations");
+        console.log(database);
 
 
-//DELETE API
+        //GET API
+        app.get('/destinations', async (req, res) => {
+            const cursor = destinationCollection.find({});
+            const destinations = await cursor.toArray();
+            res.json(destinations);
+
+        })
+
+        //POST API
 
 
-//UPDATE API
+        //DELETE API
+
+
+        //UPDATE API
+
+    }
+    finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir);
 
 
 //ROOT
